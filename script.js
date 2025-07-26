125d20cca32c37a18af4cddf0f66ef89e95cc21c
kumaraswami7862 <kumaraswami7862@gmail.com>
2025-07-26 14:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const targetElement = document.getElementById("level");
+// console.log(targetElement);
+// console.dir(targetElement);
+
+function getLevel(element) {
+    let level = 0;
+
+    while (element) {
+        element = element.parentElement;
+        level++;
+    }
+
+    return level;
+}
+
+const getDOMLevel = getLevel(targetElement);
+// console.log(getDOMLevel);
+
+alert(`The level of the element is ${getDOMLevel}`);
